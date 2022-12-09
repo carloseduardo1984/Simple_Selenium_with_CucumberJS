2c855818c21ef148364cfd803f9b63bd52204055
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2022-12-09 14:12:32 +0000

    Update TC1.js

--- features/step-definitions/TC1.js
+++ features/step-definitions/TC1.js
@@ -1,19 +1,18 @@
 const {Given, Then, When, Before, After} = require('@cucumber/cucumber')
 
 
-Given('usuario acessa menu de cadastro novamente', function () {
-    // Write code here that turns the phrase above into concrete actions
-    
-  });
+     Given('usuario acessa menu de cadastro', function () {
+       // Write code here that turns the phrase above into concrete actions
+       
+     });
 
 
-When('preenche campos com exemplos {string},{string},{string} e confirmacao', function (string, string2, string3) {      
-    // Write code here that turns the phrase above into concrete actions
-    
-  });
+     Then('o sistema exibe uma mensagem de sucesso', function () {
+       // Write code here that turns the phrase above into concrete actions
+      
+     });
 
-
-  Then('o sistema exibe uma mensagem de sucesso para cada exemplo', function () {
-    // Write code here that turns the phrase above into concrete actions
-    
-  });
+     When('preenche campos nome,email,senha e confirmacao', function () {
+        // Write code here that turns the phrase above into concrete actions
+        
+      });
